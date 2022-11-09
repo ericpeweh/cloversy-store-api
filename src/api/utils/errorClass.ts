7b23b6d5b007c90d2e3a0873163902724d407641
Eric Prima Wijaya <ericpeweh@gmail.com>
2022-11-09 22:46:52 +0700

    created error handling class utils

---
+++ src/api/utils/errorClass.ts
@@ -0,0 +1,32 @@
+class ErrorBase extends Error {
+	code: number;
+
+	constructor(
+		message: string = "Something went wrong!",
+		code: number,
+		options: { [key: string]: any } = {}
+	) {
+		super(message);
+		this.code = code;
+
+		Object.entries(options).forEach(([key, value]) => {
+			this[key as keyof Error] = value;
+		});
+	}
+
+	get statusCode() {
+		return this.code;
+	}
+}
+
+export class ClientError extends ErrorBase {
+	constructor(message: string, code: number = 400, options?: { [key: string]: any }) {
+		super(message, code, options);
+	}
+}
+
+export class ServerError extends ErrorBase {
+	constructor(message: string, code: number = 500, options?: { [key: string]: any }) {
+		super(message, code, options);
+	}
+}

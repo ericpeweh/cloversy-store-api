921d52672215e70d62e6baeb472ee5ba0f8c2a67
Eric Prima Wijaya <ericpeweh@gmail.com>
2023-01-12 12:57:17 +0700

    setup scheduler with node-scheduler

--- src/api/utils/index.ts
+++ src/api/utils/index.ts
@@ -3,3 +3,4 @@ export { default as getLocalTime } from "./getLocalTime";
 export { default as generateUniqueId } from "./generateUniqueId";
 export { default as generateUpdateQuery } from "./generateUpdateQuery";
 export { default as isDateSurpassToday } from "./isDateSurpassToday";
+export { default as scheduler } from "./scheduler";

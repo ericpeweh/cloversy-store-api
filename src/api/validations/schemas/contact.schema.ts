a34d128fdb189dc5e0308ebe7489f8ee4ab4610e
Eric Prima Wijaya <ericpeweh@gmail.com>
2023-03-25 10:06:41 +0700

    fix contact schema for title length

--- src/api/validations/schemas/contact.schema.ts
+++ src/api/validations/schemas/contact.schema.ts
@@ -5,6 +5,6 @@ export const postCreateMessageWebFormBodySchema = Joi.object({
 	senderName: Joi.string().max(255).required(),
 	email: Joi.string().email().required(),
 	objective: Joi.string().valid("Produk", "Website / Store", "Partnership", "Lainnya").required(),
-	title: Joi.string().max(30).required(),
+	title: Joi.string().max(255).required(),
 	message: Joi.string().max(1000).required()
 });

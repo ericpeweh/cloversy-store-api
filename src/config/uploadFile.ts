3782e97b31a08ee0dddaa27de154bc5622506471
Eric Prima Wijaya <ericpeweh@gmail.com>
2023-03-27 14:42:40 +0700

    remove file ext validation (use mimetype instead)

--- src/config/uploadFile.ts
+++ src/config/uploadFile.ts
@@ -12,16 +12,12 @@ const createMulterMiddleware = () => {
 			fileSize: 10 * 1024 * 1024
 		},
 		fileFilter: (_, file, callback) => {
-			const fileExt = path.extname(file.originalname);
-
 			const isValidMimeTypes =
 				file.mimetype == "image/png" ||
 				file.mimetype == "image/jpg" ||
 				file.mimetype == "image/jpeg";
 
-			const isValidExtension = fileExt === ".png" || fileExt === ".jpg" || fileExt === ".jpeg";
-
-			if (isValidMimeTypes && isValidExtension) {
+			if (isValidMimeTypes) {
 				callback(null, true);
 			} else {
 				callback(null, false);

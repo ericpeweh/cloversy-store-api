893b49fb53eeb2ca803eda504396457eb78780f6
Eric Prima Wijaya <ericpeweh@gmail.com>
2023-07-06 11:10:24 +0700

    prepare for midtrans prod integration

--- src/config/midtrans.ts
+++ src/config/midtrans.ts
@@ -6,12 +6,17 @@ dotenv.config();
 
 const coreAPI = new midtransClient.CoreApi({
 	/* 
-    SHOULD BE CHANGED TO TRUE (IN PRODUCTION, CURERNTLY ON STAGING 16/05/2023)
+    SHOULD BE CHANGED TO TRUE (IN PRODUCTION, CURERNTLY ON PRODUCTION 06/07/2023)
   */
-	isProduction: false,
-	// isProduction: process.env.NODE_ENV === "production",
-	serverKey: process.env.MIDTRANS_SERVER_KEY,
-	clientKey: process.env.MIDTRANS_CLIENT_KEY
+	isProduction: process.env.NODE_ENV === "production",
+	serverKey:
+		process.env.NODE_ENV === "production"
+			? process.env.MIDTRANS_SERVER_PRODUCTION_KEY
+			: process.env.MIDTRANS_SERVER_SANDBOX_KEY,
+	clientKey:
+		process.env.NODE_ENV === "production"
+			? process.env.MIDTRANS_CLIENT_PRODUCTION_KEY
+			: process.env.MIDTRANS_CLIENT_SANDBOX_KEY
 });
 
 export default coreAPI;

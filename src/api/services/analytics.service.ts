3ea4aad88ea83f788a75900a0b53f554748843a6
Eric Prima Wijaya <ericpeweh@gmail.com>
2023-07-21 21:58:07 +0700

    remove unused analytic function

--- src/api/services/analytics.service.ts
+++ src/api/services/analytics.service.ts
@@ -1,35 +1,6 @@
 // Dependencies
 import googleAnalytics from "../../config/googleAnalytics";
 
-export const getMonthlyWebsiteVisitorCount = async (yearFilter: string) => {
-	let analyticYear = yearFilter;
-	if (!yearFilter) {
-		analyticYear = new Date().getFullYear().toString();
-	}
-
-	const [res] = await googleAnalytics.runReport({
-		property: `properties/${process.env.GOOGLE_ANALYTICS_PROPERTY_ID!}`,
-		dateRanges: [
-			{
-				startDate: `${analyticYear}-01-01`,
-				endDate: `${analyticYear}-12-31`
-			}
-		],
-		dimensions: [
-			{
-				name: "month"
-			}
-		],
-		metrics: [
-			{
-				name: "screenPageViews"
-			}
-		]
-	});
-
-	return res?.rows || [];
-};
-
 export const getPageMonthlyVisitorAnalytics = async (pagePath: string, yearFilter: string) => {
 	let analyticYear = yearFilter;
 	if (!yearFilter) {

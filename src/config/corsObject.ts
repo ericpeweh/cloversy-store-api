b1ca2c37b97dd616794b8d68308b78826e64bfc0
Eric Prima Wijaya <ericpeweh@gmail.com>
2023-07-22 11:10:56 +0700

    formatting

--- src/config/corsObject.ts
+++ src/config/corsObject.ts
@@ -6,10 +6,10 @@ const corsObject = {
 	origin:
 		process.env.NODE_ENV === "production"
 			? [
-				"https://admin.cloversy.id",
-				"https://www.admin.cloversy.id",
-				"https://cloversy.id",
-				"https://www.cloversy.id"
+					"https://admin.cloversy.id",
+					"https://www.admin.cloversy.id",
+					"https://cloversy.id",
+					"https://www.cloversy.id"
 			  ]
 			: ["http://localhost:3000", "http://localhost:3001"],
 	methods: ["GET", "POST", "PUT", "PATCH", "OPTIONS", "HEAD", "DELETE"],
